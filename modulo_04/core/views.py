2e3a7697fd91f65e82fe021f4a39a1ade568662a
Miguel <nigelkps1@gmail.com>
2025-11-07 19:00:15 -0300

    Solução completa da Apostila 3 (Models, Admin e Template)

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Tarefa
 
-# Create your views here.
 def home(request):
+    todas_as_tarefas = Tarefa.objects.all()
+
     context = {
         'nome_usuario': 'Júnior',
-        'tecnologias': ['Python', 'Django', 'HTML', 'CSS'],
+        'tecnologias': ['Python', 'Django', 'Models', 'Admin'],
+        'tarefas': todas_as_tarefas
     }
-    # return HttpResponse("<h1>Olá, Mundo! Esta é minha primeira página Django!</h1>")
-    return render(request, 'home.html', context)
 
-def login(request):
-    return HttpResponse("<input>Login</input>")
+    return render(request, 'home.html', context)
