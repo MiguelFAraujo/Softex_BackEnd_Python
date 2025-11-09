91cffc55edac021b3201d53a2cfac5de796f2148
Miguel <nigelkps1@gmail.com>
2025-11-09 12:16:09 -0300

    Gabarito da Apostila 4 (Forms) concluído

--- modulo_04/core/views.py
+++ modulo_04/core/views.py
@@ -1,13 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Tarefa
+from .forms import TarefaForm
 
 def home(request):
-    todas_as_tarefas = Tarefa.objects.all()
+
+    if request.method == 'POST':
+        
+        form = TarefaForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect('home')
+    else:
+
+        form = TarefaForm() 
+
+    todas_as_tarefas = Tarefa.objects.all().order_by('-criada_em')
 
     context = {
         'nome_usuario': 'Júnior',
-        'tecnologias': ['Python', 'Django', 'Models', 'Admin'],
-        'tarefas': todas_as_tarefas
+        'tecnologias': ['Python', 'Django', 'Models', 'Forms'],
+        'tarefas': todas_as_tarefas,
+        'form': form
     }
 
     return render(request, 'home.html', context)
