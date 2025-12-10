a7f8caafddb8eca3aa0c3a1dbd7cb886d70b917c
Miguel <nigelkps1@gmail.com>
2025-12-10 15:19:50 -0300

    Update views

--- modulo_05/core/views.py
+++ modulo_05/core/views.py
@@ -1,9 +1,12 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from django.shortcuts import get_object_or_404
+
+#adições necessárias para o POST(ap3)
 from .models import Tarefa
 from .serializers import TarefaSerializer
-from django.shortcuts import get_object_or_404
+
 
 class ListaTarefasAPIView(APIView):
     """
