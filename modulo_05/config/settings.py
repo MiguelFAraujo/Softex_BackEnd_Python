3cded831a4eaa8679bc2474210f4bdd463ecbd92
Anderson Costa Rodrigues <110496377+AndersonCRodrigues@users.noreply.github.com>
2026-01-05 11:00:51 -0300

    start test drf

--- modulo_05/config/settings.py
+++ modulo_05/config/settings.py
@@ -13,6 +13,7 @@ import os
 import environ
 from pathlib import Path
 from datetime import timedelta
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -152,3 +153,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+if 'test' in sys.argv: 
+    DATABASES['default'] = { 
+    'ENGINE': 'django.db.backends.sqlite3', 
+    'NAME': ':memory:',
+} 
